6de6f62dc16126199b0c8adfb27891102f7ce91d
Vijay Papanaboina <vijaypapanaboina3@gmail.com>
2025-04-02 23:21:38 +0530

    removed extra messages route

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -8,61 +8,19 @@ export default (pool) => {
 
 
 
-  router.get("/auth-messages", authentication , async (req,res) => {
-    const userId  = req.user.id;
-    const chatUserId = req.query.chatUserId;
-    console.log("user",userId);
-
-    try {
-      const result = await pool.query(
-        `SELECT * FROM messages
-        WHERE (sender = $1 AND receiver = $2) OR (sender = $2 AND receiver = $1)
-        ORDER BY created_at DESC`,
-        [userId,chatUserId]
-      );
-      res.status(200).json(result.rows);
-    } catch (err) {
-      console.error("Error getting user:", err);
-      res.status(500).json({ success: false, error: "Internal Server Error" });
-    }
-  })
-
-
-
 
   // Get messages between two users with offset-based pagination
   router.get("/messages", authentication , async (req, res) => {
-    const { chatWith, offset, limit } = req.query;
+    const { chatWith, offset = 0, limit = 50 } = req.query;
     const currentUser = req.user.id; // Assuming you have user ID in the request
-    const offsetVal = parseInt(offset, 10) || 0;
-    const limitVal = parseInt(limit, 10) || 50;
     try {
-      console.log(`Loading messages: offset=${offsetVal}, limit=${limitVal}`);
+      console.log(`Loading messages: offset=${offset}, limit=${limit}`);
       const query = `
         SELECT * FROM messages
         WHERE (sender = $1 AND receiver = $2) OR (sender = $2 AND receiver = $1)
         ORDER BY created_at DESC
         LIMIT $3 OFFSET $4;
       `;
-      const values = [chatWith, currentUser, limitVal, offsetVal];
-      const result = await pool.query(query, values);
-      res.json(result.rows);
-    } catch (error) {
-      console.error("Error fetching messages:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  });
-
-  router.get("more-/messages", authentication , async (req, res) => {
-    const { chatWith, offset = 0, limit = 50 } = req.query;
-    const currentUser = req.user.id; // Assuming you have user ID in the request
-    try {
-      const query = `
-      SELECT * FROM messages
-      WHERE (sender = $1 AND receiver = $2) OR (sender = $2 AND receiver = $1)
-      ORDER BY created_at DESC
-      LIMIT $3 OFFSET $4;
-    `;
       const values = [chatWith, currentUser, limit, offset];
       const result = await pool.query(query, values);
       res.json(result.rows);
@@ -71,6 +29,7 @@ export default (pool) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
   });
+
 
   // Get chat user details endpoint
   router.get("/get-chat-user", async (req, res) => {
